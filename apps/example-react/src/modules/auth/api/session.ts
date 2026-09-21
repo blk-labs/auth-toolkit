b06e74fb27e9f940ed6f6fb7af5ef7175f5a6f59
Joxeph21 <codewithjoxeph@gmail.com>
2026-09-21 10:24:38 +0100

    fast-forward

---
+++ apps/example-react/src/modules/auth/api/session.ts
@@ -0,0 +1,14 @@
+import { http, HttpResponse, type HttpHandler, type PathParams } from 'msw';
+import usersData from '@/shared/constants/users.json';
+import axios from 'axios';
+import type { AuthResponse, LoginRequestBody } from '../types';
+import { ThrowError } from '../utils/helper';
+
+
+export const sessionHandler: HttpHandler = http.get<PathParams>('/api/session', async () => {
+  return HttpResponse.json({
+    status: 200,
+    message: 'success',
+    data: ,
+  });
+});
